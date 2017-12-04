a1484d76cbece2a7e19053fad2d04286deed44d7
Nayib <an.murillo10@uniandes.edu.co>
2017-12-03 23:01:08 -0500

    Desarrollo del makefile

---
+++ Tarea5.mk
@@ -0,0 +1,10 @@
+Resultados_hw5.pdf : plot.png
+	pdflatex Resultados_hw5.tex
+	rm Resultados_hw5.log Resultados_hw5.aux plot.png dato.dat bayfit.dat curvaRotacion.o
+
+plot.png : dato.dat bayfit.dat
+	python Plots.py
+
+dato.dat bayfit.dat: CurvaRotacion.c
+	cc -o curvaRotacion.o CurvaRotacion.c -lm
+	./curvaRotacion.o
